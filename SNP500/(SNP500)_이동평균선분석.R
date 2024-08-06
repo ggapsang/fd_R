b2a8e95ea4a5172c294cad832f3841e152aa41c4
sanghyun lee <58983068+ggapsang@users.noreply.github.com>
2024-08-06 12:45:10 +0900

    Update and rename (sp500)_이동평균선분석.R to (SNP500)_이동평균선분석.R

--- SNP500/(SNP500)_이동평균선분석.R
+++ SNP500/(SNP500)_이동평균선분석.R
@@ -1,17 +1,19 @@
+# packages used
 # install.packages("quantmod")
 # install.packages("PerformanceAnalytics")
+
 
 # Load library
 library(quantmod)
 library(PerformanceAnalytics)
 
+
 # Load Data
 getSymbols("^GSPC", src="yahoo", from="1991-01-01", to= Sys.Date())
-
 tail(GSPC, 10)
 
-# Visualization of S&P500 SMA
 
+# Visualization of S&P500 SMA
 graphics.off()
 
 chartSeries(GSPC, 
@@ -27,11 +29,35 @@ addSMA(n = 200, col = "red")
 # Visualization only moving averages
 graphics.off()
 
-sma_50 = SMA(Cl(GSPC), n=50)
-sma_200 = SMA(Cl(GSPC), n=200)
+cl_GSPC <- Cl(GSPC)
+sma_50 <- SMA(Cl(GSPC), n=50)
+sma_200 <- SMA(Cl(GSPC), n=200)
 
 plot(index(sma_50), coredata(sma_50), type = "l", col = "blue",
      main = "50-day and 200-day Moving Averages",
-     xlab = "Date", ylab = "Value",
+     xlab = "Year", ylab = "Value",
      xaxt = 'n')  # 기본 x축 레이블 제거
 lines(index(sma_200), coredata(sma_200), col = "red")
+lines(index(GSPC), coredata(cl_GSPC), col = "gray")
+
+
+# Add label 'year'
+
+years <- format(index(GSPC), "%Y")
+unique_years <- unique(years)
+yearly_first_indexes <- sapply(unique_years, function(y) {
+  min(which(years == y))
+})
+
+axis(1, 
+     at=index(GSPC)[yearly_first_indexes], 
+     labels=unique_years, 
+     las=2, 
+     cex.axis=0.7, 
+     tick=TRUE)
+
+# Add legend
+legend("topleft", 
+       legend=c("50-day SMA", "200-day SMA", "SNP500"), 
+       col=c("blue", "red", "gray"), 
+       lty=1)
