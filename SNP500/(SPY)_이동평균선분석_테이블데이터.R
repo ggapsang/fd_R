035cc3fa80c67117354fc09af8f5bfcc5353614d
DESKTOP-HVH1CSC\ASUS <ggapsang@gmail.com>
2024-08-07 00:21:06 +0900

    함수로 변환(analysis_sma_crossover)

--- SNP500/(SPY)_이동평균선분석_테이블데이터.R
+++ SNP500/(SPY)_이동평균선분석_테이블데이터.R
@@ -5,35 +5,31 @@
 library(quantmod)
 library(PerformanceAnalytics)
 
-# LOAD DATA
-getSymbols("SPY", src = "yahoo", from = "1993-01-29")
+analysis_moving_average <- function(ticker_code, start_date){
+  
+  # LOAD DATA
+  getSymbols(ticker_code, src = "yahoo", from = start_date)
+  df <- get(ticker_code)
 
-tail(SPY)
+  # ADD A COLUMN OF SMA50
+  df$SMA50 <- SMA(Cl(df), 50)
+  # ADD A COLUMN OF SMA200
+  df$SMA200 <- SMA(Cl(df), 200)
+  
+  # CHECK BULL OR BEAR MARKET
+  df$MarketTrend <- ifelse(df$SMA50 - df$SMA200 > 0, TRUE, FALSE) # BULL MARKET = 1, BEAR MARKET = 0
 
-# ADD A COLUMN OF SPY.SMA50
-SPY$SPY.SMA50 <- SMA(Cl(SPY), 50)
+  # CHCEK CROSSOVER
+  # GOLDEN CROSS 0 -> 1
+  df$GoldenCross <- ifelse(df$MarketTrend == TRUE & lag(df$MarketTrend, 1) == FALSE, 1, 0)
+  # DEATH CROSS 1 -> 0
+  df$DeathCross <- ifelse(df$MarketTrend == FALSE & lag(df$MarketTrend, 1) == TRUE, 1, 0)
+  
+  # GET THE DATES OF GOLDEN CROSS
+  print(index(df[df$GoldenCross == 1, ]))
+  
+  # GET THE DATES OF DEAD CROSS
+  print(index(df[df$DeathCross == 1, ]))
 
-# ADD A COLUMN OF SPY.SMA200
-SPY$SPY.SMA200 <- SMA(Cl(SPY), 200)
-
-# CHECK BULL OR BEAR MARKET
-#SPY <- na.omit(SPY)
-sum(is.na(SPY$SPY.SMA50))
-sum(is.na(SPY$SPY.SMA200))
-SPY$SPY.MarketTrend <- ifelse(SPY$SPY.SMA50 - SPY$SPY.SMA200 > 0, TRUE, FALSE) # BULL MARKET = 1, BEAR MARKET = 0
-
-tail(SPY)
-
-# CHECK CROSSOVER
-# Golden crossover 0 -> 1
-SPY$SPY.GoldenCross <- ifelse(SPY$SPY.MarketTrend == TRUE & lag(SPY$SPY.MarketTrend, 1) == FALSE, 1, 0)
-# Death crossover 1 -> 0
-SPY$SPY.DeathCross <- ifelse(SPY$SPY.MarketTrend == FALSE & lag(SPY$SPY.MarketTrend, 1) == TRUE, 1, 0)
-
-tail(SPY)
-
-# GET THE DATES OF GOLDEN CROSS
-print(index(SPY[SPY$SPY.GoldenCross == 1, ]))
-
-# GET THE DATES OF DEAD CROSS
-print(index(SPY[SPY$SPY.DeathCross == 1, ]))
+  return(df)
+}
